860a93fea8a8c19fcd7dd47319d0e98dbc8070cb
Gaurav <gaurav.mg91@gmail.com>
2018-04-27 17:18:56 -0400

    added profile listening and creating

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 from . import serializers
+from . import models
 
 # Create your views here.
 
@@ -120,3 +121,11 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles removing an object."""
 
         return Response ({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+    """handles creating updating reading and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+
+    queryset = models.UserProfile.objects.all()
