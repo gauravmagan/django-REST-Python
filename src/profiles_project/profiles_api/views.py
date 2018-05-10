4a73baebfd91f6385d1f910ba90bf1aa2df9372f
Gaurav <gaurav.mg91@gmail.com>
2018-05-10 14:53:52 -0400

    first

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -9,6 +9,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 
 from . import serializers
@@ -152,3 +154,16 @@ class LoginViewSet(viewsets.ViewSet):
         """use the obtainauthtoken APIVIEW to validate and create a token """
 
         return ObtainAuthToken().post(request)
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """ Handles creating , reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+
+        """ Sets the user profile to the logged user."""
+        serializer.save(user_profile=self.request.user)
